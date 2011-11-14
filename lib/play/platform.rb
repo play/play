a2e48e96ce90685f3794c769bfc2736c8b0b9603
Tom Bell <tomb@tombell.org.uk>
2011-11-14 15:22:57 -0500

    Abstract out binary commands

--- lib/play/platform.rb
+++ lib/play/platform.rb
@@ -1,16 +1,72 @@
 module Play
   class Platform
+    # Tests if currently running on Darwin.
+    #
+    # Returns true if platform is Darwin (Mac OS X), else false.
     def self.darwin?
       !!(RUBY_PLATFORM =~ /darwin/)
     end
 
+    # Tests if currently running on Linux.
+    #
+    # Returns true if platform is Linux, else false.
     def self.linux?
       !!(RUBY_PLATFORM =~ /linux/)
     end
 
+    # Tests if currently running on Windows.
+    #
+    # Returns true if platform is Windows, else false.
     def self.windows?
-      warn("Windows is not currently supported!")
       !!(RUBY_PLATFORM =~ /mswin|mingw/)
+    end
+
+    # Gets the command used to play a music file for the current platform.
+    #
+    # Returns a String of the command.
+    def self.play_command
+      if darwin?
+        'afplay'
+      elsif linux?
+        'play'
+      elsif windows?
+        warn('You are using an unsupported operating system')
+        exit(1)
+      end
+    end
+
+    # Gets the command used to say something over the speakers on the current
+    # platform.
+    #
+    # Returns a String of the command.
+    # TODO: Find something like `say` for Linux.
+    def self.say_command
+      if darwin?
+        'say'
+      elsif linux?
+        ''
+      elsif windows?
+        warn('You are using an unsupported operating system')
+        exit(1)
+      end
+    end
+
+    # Get the command used to change the volume on the current platform.
+    #
+    # number - An Integer of the volume to change to.
+    #
+    # Returns a String of the command.
+    def self.volume_command(number)
+      number = 10 if number > 10
+      if darwin?
+        "osascript -e 'set volume #{number}' 2>/dev/null"
+      elsif linux?
+        volume = number.to_i * 10
+        "amixer set Master #{volume}%"
+      elsif windows?
+        warn('You are using an unsupported operating system')
+        exit(1)
+      end
     end
   end
 end
--- test/platform_test.rb
+++ test/platform_test.rb
@@ -13,8 +13,40 @@ context "Platform" do
   end
 
   test "windows?" do
-    Platform.stubs(:warn).returns(true)
     assert_equal RUBY_PLATFORM.include?('mswin') ||
                  RUBY_PLATFORM.include?('mingw'), Play::Platform.windows?
+  end
+
+  test "play_command" do
+    Play::Platform.stubs(:warn).returns(true)
+    assert_equal "afplay", Play::Platform.play_command if RUBY_PLATFORM.include?('darwin')
+    assert_equal "play", Play::Platform.play_command if RUBY_PLATFORM.include?('linux')
+
+    assert_equal 1, Play::Platform.play_command if RUBY_PLATFORM.include?('mswin') ||
+                                                   RUBY_PLATFORM.include?('mingw')
+  end
+
+  test "say_command on darwin" do
+    if RUBY_PLATFORM =~ /darwin/
+      assert_equal "say", Play::Platform.say_command
+    end
+  end
+
+#  test "say_command on linux" do
+#    if RUBY_PLATFORM =~ /linux/
+#      assert_equal "", Play::Platform.say_command
+#    end
+#  end
+
+  test "volume_command on darwin" do
+    if RUBY_PLATFORM =~ /darwin/
+      assert_equal "osascript -e 'set volume 2' 2>/dev/null", Play::Platform.volume_command(2)
+    end
+  end
+
+  test "volume_command on linux" do
+    if RUBY_PLATFORM =~ /linux/
+      assert_equal "amixer set Master 20%", Play::Platform.volume_command(2)
+    end
   end
 end
