4b4476cdcef4b1b9f71341dba06bdeaa1224c2f5
Zach Holman <zach@zachholman.com>
2012-01-03 01:18:04 -0600

    Remove more code

--- app/views/layout.rb
+++ app/views/layout.rb
@@ -2,36 +2,8 @@ module Play
   module Views
     class Layout < Mustache
       
-      def queue_page
-        request.env["REQUEST_URI"] =~ /\/$/ ? 'active' : ''
-      end
-
-      def history_page
-        request.env["REQUEST_URI"] =~ /\/history$/ ? 'active' : ''
-      end
-
-      def profile_page
-        request.env["REQUEST_URI"] =~ /\/#{@login}$/ ? 'active' : ''
-      end
-
-      def stream?
-        !stream_url.blank?
-      end
-
-      def stream_url
-        Play.config['stream_url']
-      end
-
-      def playing?
-        Player.paused?
-      end
-
       def now_playing
         Player.now_playing
-      end
-
-      def login
-        @login
       end
 
     end
