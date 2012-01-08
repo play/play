c9f4e57d843ffd1a5e3c6521793371f3910e79b0
Zach Holman <zach@zachholman.com>
2012-01-08 14:44:22 -0800

    TomDoc-driven development

---
+++ app/models/history.rb
@@ -0,0 +1,32 @@
+module Play
+
+  # Keeps track of the historical record of Play. Think plays and user votes and
+  # playcounts, oh my.
+  class History
+    
+    # Public: Add a play history for a song.
+    #
+    # song - A Song.
+    # user - A User.
+    #
+    # Returns nothing important.
+    def self.add(song,user)
+    end
+
+    # Public: The total playcount of the library. Ever.
+    #
+    # Returns an Integer.
+    def self.count
+    end
+
+    # Public: The last x listens, with latest on top.
+    #
+    # number - The Integer number of results we should return.
+    #
+    # Returns an Array of Songs.
+    def self.last(number=1)
+    end
+
+  end
+
+end
