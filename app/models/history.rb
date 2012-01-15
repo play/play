00fb9c8bed58426b14a6601f1b192d76c60d25d0
Zach Holman <zach@zachholman.com>
2012-01-14 19:02:13 -0800

    Expand redis docs

--- app/models/history.rb
+++ app/models/history.rb
@@ -4,12 +4,20 @@ module Play
   #
   # redis:
   #
-  #   play:histories:ids            - A List of all song IDs played.
-  #   play:histories:songs          - A Sorted Set of all songs played. Values:
-  #                                     - ID
-  #                                     - Date
-  #   play:histories:#{login}:ids   - A List of all song IDs played by the user.
-  #   play:histories:#{login}:songs - A Sorted Set of all songs played by user.
+  #   Histories are stored in two places, in two scopes: a dumb list for the
+  #   sake of quickly pulling an ordered Array of integers, and a smart Sorted
+  #   Set so we can get more information about that particular history item.
+  #
+  #   The Sorted Sets contain the following:
+  #
+  #     id, created_at (the "date played"), login (if relevant)
+  #
+  #   The keys are stored in redis as follows:
+  #
+  #     play:histories:ids            - A List of all song IDs played.
+  #     play:histories:songs          - A Sorted Set of all songs played.
+  #     play:histories:#{login}:ids   - A List of song IDs played by the user.
+  #     play:histories:#{login}:songs - A Sorted Set of songs played by user.
   class History
     # The redis key to stash History data.
     KEY = 'play:histories'
