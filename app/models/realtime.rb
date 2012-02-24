9d14c4a2f96bfd5fbeadfa2b2b66effa89ffae47
Joey Wendt <joey@joeywendt.com>
2012-02-24 15:28:31 -0600

    Remove pusher wrapper method. Call pusher directly.

--- app/models/realtime.rb
+++ app/models/realtime.rb
@@ -3,25 +3,17 @@ module Play
   # Realtime handles pushing updates to Pusher which in turn updates the
   # play web client.
   class Realtime
-
-    # Public: Pushes an update event to Pusher.
-    # 
-    # channel - String channel the event is sent to.
-    # event - String event that is triggered.
-    # data - JSON string containing data associated with the event.
-    #
-    def self.push_update(channel, event, data)
-      Pusher[channel].trigger(event, data)
-    rescue Pusher::Error => e
-    end
       
     # Public: Sends updated now playing song and queue.
     #
     # now_playing - Song we want the web client to say is now playing.
     #
+    # Returns true/false based on success.
     def self.update_now_playing(now_playing)
       song = Yajl.dump now_playing.to_hash
-      push_update("now_playing_updates", "update_now_playing", song)
+      Pusher["now_playing_updates"].trigger("update_now_playing", song)
+    rescue Pusher::Error => e
+      false
     end
 
   end
