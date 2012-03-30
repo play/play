24535c01ee325620fb8dd968b96f899ed0fab616
Zach Holman <zach@zachholman.com>
2012-03-29 18:57:32 -0700

    Sane spacing

--- app/api/speaker.rb
+++ app/api/speaker.rb
@@ -20,54 +20,54 @@ module Play
     end
 
     get "/speakers" do
-    	speakers = {
-    		:speakers => Airfoil.get_speakers.map { |speaker| speaker.to_hash }
-    	}.to_json
+      speakers = {
+        :speakers => Airfoil.get_speakers.map { |speaker| speaker.to_hash }
+      }.to_json
     end
 
     get "/speaker/:id" do
       validate_speaker_id(params[:id])
 
-    	speaker = {
-    		:speaker => Speaker.new(params[:id]).to_hash
-    	}.to_json
+      speaker = {
+        :speaker => Speaker.new(params[:id]).to_hash
+      }.to_json
     end
 
     put "/speaker/:id/volume" do
       validate_speaker_id(params[:id])
 
-    	speaker = Speaker.new params[:id]
-    	speaker.volume = params[:volume]
-    	speaker = {
-    		:speaker => speaker.to_hash
-    	}.to_json
+      speaker = Speaker.new params[:id]
+      speaker.volume = params[:volume]
+      speaker = {
+        :speaker => speaker.to_hash
+      }.to_json
     end
 
     put "/speaker/:id/connect" do
       validate_speaker_id(params[:id])
 
-    	speaker = Speaker.new params[:id]
-    	speaker.connect!
-    	speaker = {
-    		:speaker => speaker.to_hash
-    	}.to_json
+      speaker = Speaker.new params[:id]
+      speaker.connect!
+      speaker = {
+        :speaker => speaker.to_hash
+      }.to_json
     end
 
     put "/speaker/:id/disconnect" do
       validate_speaker_id(params[:id])
 
-    	speaker = Speaker.new params[:id]
-    	speaker.disconnect!
-    	speaker = {
-    		:speaker => speaker.to_hash
-    	}.to_json
+      speaker = Speaker.new params[:id]
+      speaker.disconnect!
+      speaker = {
+        :speaker => speaker.to_hash
+      }.to_json
     end
 
     put "/volume" do
-    	Airfoil.speakers_volume = params[:volume]
-    	speakers = {
-    		:speakers => Airfoil.get_speakers.map { |speaker| speaker.to_hash }
-    	}.to_json
+      Airfoil.speakers_volume = params[:volume]
+      speakers = {
+        :speakers => Airfoil.get_speakers.map { |speaker| speaker.to_hash }
+      }.to_json
     end
 
   end
