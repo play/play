3ddcb039dcaa024e644ffe7f88869ba5737055fe
Zach Holman <zach@zachholman.com>
2012-09-18 15:19:11 -0700

    Pull in libraries

--- Gemfile
+++ Gemfile
@@ -1,1 +1,3 @@
 source 'http://rubygems.org'
+
+gem 'library', '0', :git => "https://github.com/play/library"
---
+++ lib/play.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.setup(:default)
+Bundler.require(:default)
+
+module Play
+end
