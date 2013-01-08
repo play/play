5fbf0a516550b6a21f0905691b1cab9f002e3eca
Zach Holman <zach@zachholman.com>
2013-01-07 20:13:54 -0800

    Song upload endpoint

--- app/app.rb
+++ app/app.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 module Play
   class App < Sinatra::Base
     # Include our Sinatra Helpers.
@@ -144,6 +146,25 @@ module Play
 
     delete "/like" do
       current_user.dislike(params[:path])
+    end
+
+    post '/upload' do
+      params[:files].each do |file|
+        tmpfile = file[:tempfile]
+        name    = file[:filename].chomp.delete("\000")
+
+        # taglib needs a file extension (lol)
+        new_tmp = File.join(File.dirname(tmpfile), File.basename(name))
+        File.rename(tmpfile.path, new_tmp)
+        song    = Song.new(new_tmp)
+
+        # Import into our collection
+        path = File.join(Play.music_path, song.artist_name, song.album_name)
+        FileUtils.mkdir_p(path)
+        File.rename song.path, File.join(path, File.basename(song.path))
+      end
+
+      true
     end
   end
 end
