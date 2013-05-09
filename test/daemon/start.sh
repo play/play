18198c13122412ee2e6967bb29198d80a49e91e8
Zach Holman <zach@zachholman.com>
2013-05-08 18:20:57 -0700

    Supress output again

--- test/daemon/start.sh
+++ test/daemon/start.sh
@@ -2,5 +2,5 @@
 #
 # Spin up our separate testing-only mpd and then kill it after ten seconds
 # (which hopefully is as long a test suite as we'll see... maybe).
-mpd test/daemon/mpd.conf #> /dev/null 2>&1
+mpd test/daemon/mpd.conf > /dev/null 2>&1
 # { sleep 10; kill $(cat /tmp/play-test/.mpd/pid); } &
