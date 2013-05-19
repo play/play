1c1df5a9f129e99f8e0e5da3c29f2329c23b96a7
Zach Holman <zach@zachholman.com>
2013-05-19 16:10:49 -0500

    Move upload endpoint over

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,6 +4,25 @@ class SongsController < ApplicationController
     @song   = @artist.songs.find{|song| song.title == CGI.unescape(params[:title])}
   end
 
+  def create
+    params[:files].each do |file|
+      tmpfile = file[:tempfile]
+      name    = file[:filename].chomp.delete("\000")
+
+      # taglib needs a file extension (lol)
+      new_tmp = File.join(File.dirname(tmpfile), File.basename(name))
+      File.rename(tmpfile.path, new_tmp)
+      song    = Song.new(new_tmp)
+
+      # Import into our collection
+      path = File.join(Play.music_path, song.artist_name, song.album_name)
+      FileUtils.mkdir_p(path)
+      File.rename song.path, File.join(path, File.basename(song.path))
+    end
+
+    true
+  end
+
   def search
     @filter = (params[:filter] || :any).to_sym
     @songs = Song.find([@filter,params[:q]])
