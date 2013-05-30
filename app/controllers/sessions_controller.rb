a6df2eb81495d309d66d5856476fcc3dc424c7dc
Jon Maddox <jon@jonmaddox.com>
2013-05-30 15:03:14 -0400

    sessions controller for signing in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  skip_before_filter :require_auth, :only => [:create, :failure]
+
+  def create
+    @user = User.find_for_github_oauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      session[:github_login] = @user.login
+      url = session[:return_to] ? session[:return_to] : root_url
+      session[:return_to] = nil
+      session.delete(:return_to)
+      redirect_to url and return
+    end
+
+    render :action => :failure
+  end
+
+  def failure
+    render :layout => false
+  end
+
+  def logout
+    session[:github_login] = ""
+    session.delete(:github_login)
+    redirect_to '/'
+  end
+
+end
