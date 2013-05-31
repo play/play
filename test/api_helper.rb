c474a29ce36d756d32e0f5d840e459f11caf5930
Jon Maddox <jon@jonmaddox.com>
2013-05-31 13:35:30 -0400

    add api tests

---
+++ test/api_helper.rb
@@ -0,0 +1,46 @@
+
+Play.config['auth_token'] = '123456789'
+
+def authorized_rack_env_for(user)
+  {"HTTP_AUTHENTICATION" => user.token}
+end
+
+def authorized_get(uri, user, opts={})
+  get uri, opts, authorized_rack_env_for(user)
+end
+
+def authorized_post(uri, user, opts={})
+  post uri, opts, authorized_rack_env_for(user)
+end
+
+def authorized_put(uri, user, opts={})
+  put uri, opts, authorized_rack_env_for(user)
+end
+
+def authorized_delete(uri, user, opts={})
+  delete uri, opts, authorized_rack_env_for(user)
+end
+
+def unauthorized_get(uri, opts={})
+  rack_env = {"HTTP_AUTHENTICATION" => "xxxxxxxxxxxxxxxxxx"}
+  get uri, opts, rack_env
+end
+
+def assert_json(response)
+  assert_equal 'application/json;charset=utf-8', response.headers["Content-Type"]
+end
+
+def parse_response(response)
+  Yajl::load(response.body)
+end
+
+def assert_song_representation(object)
+  keys = object.keys
+
+  assert_equal 5, keys.size
+  assert keys.include? 'title'
+  assert keys.include? 'artist'
+  assert keys.include? 'album'
+  assert keys.include? 'seconds'
+  assert keys.include? 'path'
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,11 @@
 ENV["RAILS_ENV"] = "test"
+ENV["RACK_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require File.expand_path(File.dirname(__FILE__) + '/blueprints')
+require File.expand_path("../api_helper", __FILE__)
+
+include Rack::Test::Methods
 
 # Set up our test mpd instance and its "music"
 system 'rm -rf   /tmp/play-test'
@@ -33,4 +37,32 @@ class Client
   def port
     '6611'
   end
+end
+
+##
+# test/spec/mini 5
+# http://gist.github.com/307649
+# chris@ozmm.org
+
+def context(*args, &block)
+  return super unless (name = args.first) && block
+  require 'test/unit'
+  klass = Class.new(defined?(ActiveSupport::TestCase) ? ActiveSupport::TestCase : Test::Unit::TestCase) do
+    def self.test(name, &block)
+      define_method("test_#{name.to_s.gsub(/\W/,'_')}", &block) if block
+    end
+    def self.xtest(*args) end
+    def self.context(*args, &block) instance_eval(&block) end
+    def self.setup(&block)
+      # Clear out the entire queue during each `setup` block.
+      Client.new.clear
+
+      define_method(:setup) { self.class.setups.each { |s| instance_eval(&s) } }
+      setups << block
+    end
+    def self.setups; @setups ||= [] end
+    def self.teardown(&block) define_method(:teardown, &block) end
+  end
+  (class << klass; self end).send(:define_method, :name) { name.gsub(/\W/,'_') }
+  klass.class_eval &block
 end
