b875be2fd1d78f0e01dce9b100ea3658530315eb
Jon Maddox <jon@jonmaddox.com>
2013-05-31 22:34:34 -0400

    do these tests now

--- test/api/controls_test.rb
+++ test/api/controls_test.rb
@@ -7,42 +7,40 @@ class ControlsTest < ActiveSupport::TestCase
       @user = User.make!
     end
 
-  #   context "/now_playing" do
-  #     test "when nothing is playing" do
-  #       authorized_get '/api/now_playing', @user
-  #       parsed_response = parse_response(last_response)
-  #
-  #       assert last_response.ok?
-  #       assert_json last_response
-  #       assert_equal Hash, parsed_response.class
-  #
-  #       keys = parsed_response.keys
-  #
-  #       assert_equal 1, keys.size
-  #       assert keys.include? 'now_playing'
-  #
-  #       assert_nil parsed_response['now_playing']
-  #     end
-  #
-  #     test "when something is playing" do
-  #       authorized_get '/api/now_playing', @user
-  #       parsed_response = parse_response(last_response)
-  #
-  #       assert last_response.ok?
-  #       assert_json last_response
-  #       assert_equal Hash, parsed_response.class
-  #
-  #       keys = parsed_response.keys
-  #
-  #       assert_equal 1, keys.size
-  #       assert keys.include? 'now_playing'
-  #
-  #       assert_nil parsed_response['now_playing']
-  #     end
-  #
-  #   end
+    context "/now_playing" do
+      test "when nothing is playing" do
+        authorized_get '/api/now_playing', @user
+        parsed_response = parse_response(last_response)
 
+        assert last_response.ok?
+        assert_json last_response
+        assert_equal Hash, parsed_response.class
 
+        keys = parsed_response.keys
+
+        assert_equal 1, keys.size
+        assert keys.include? 'now_playing'
+
+        assert_nil parsed_response['now_playing']
+      end
+
+      test "when something is playing" do
+        authorized_get '/api/now_playing', @user
+        parsed_response = parse_response(last_response)
+
+        assert last_response.ok?
+        assert_json last_response
+        assert_equal Hash, parsed_response.class
+
+        keys = parsed_response.keys
+
+        assert_equal 1, keys.size
+        assert keys.include? 'now_playing'
+
+        assert_nil parsed_response['now_playing']
+      end
+
+    end
 
   end
 end
