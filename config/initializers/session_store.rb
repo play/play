403ef27e5094c66667bffb43aba7ed963d28a5d3
Zach Holman <zach@zachholman.com>
2013-06-24 19:26:24 -0500

    keep sessions around for awhile

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Play::Application.config.session_store :cookie_store, :key => '_play_session'
+Play::Application.config.session_store :cookie_store,
+                                       :key => '_play_session',
+                                       :expire_after => 30.days
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
