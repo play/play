bd7fba8e1eed36514086c3ce8ea0f4687cc8ea0a
Zach Holman <zach@zachholman.com>
2013-06-24 19:25:29 -0500

    use real hashrockets here

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Play::Application.config.session_store :cookie_store, key: '_Play_session'
+Play::Application.config.session_store :cookie_store, :key => '_play_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
