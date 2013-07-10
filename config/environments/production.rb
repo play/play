c81789e982650fb0abb8664ad891d4682115dc45
Zach Holman <zach@zachholman.com>
2013-07-09 19:13:21 -0700

    serve assets in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,8 +8,7 @@ Play::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
