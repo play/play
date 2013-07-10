b9df097b0794ceddc3494794844a2c87c5b7ec97
Jon Maddox <jon@jonmaddox.com>
2013-07-10 16:13:07 -0400

    new PlayQueue api

--- app/controllers/api/controls_controller.rb
+++ app/controllers/api/controls_controller.rb
@@ -1,7 +1,7 @@
 class Api::ControlsController < Api::BaseController
 
   def now_playing
-    song = Song.now_playing
+    song = PlayQueue.now_playing
     deliver_json(200, {:now_playing => song.try(:to_hash)})
   end
 
