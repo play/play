f75fe3903ae2929c4b609cfe6e950f6ba5306291
Jon Maddox <jon@jonmaddox.com>
2013-07-11 16:57:02 -0400

    artists api

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,9 +1,12 @@
 class Api::ArtistsController < Api::BaseController
 
   def index
+    deliver_error(406, {:message => 'NOT IMPLEMENTED'})
   end
 
   def show
+    artist = Artist.new(:name => params[:artist_name])
+    deliver_json(200, artist.to_hash)
   end
 
 end
