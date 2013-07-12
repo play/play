f95b6836608a7c315e61f0309ef7f882d9882adb
Jon Maddox <jon@jonmaddox.com>
2013-07-12 00:21:48 -0400

    queue api

--- app/controllers/api/queue_controller.rb
+++ app/controllers/api/queue_controller.rb
@@ -10,12 +10,32 @@ class Api::QueueController < Api::BaseController
   end
 
   def add
+    case params[:type]
+    when /song/
+      artist = Artist.new(:name => params[:artist_name])
+      song = artist.songs.find{|song| song.title == params[:song_name]}
+      PlayQueue.add(song,current_user)
+    when /album/
+      artist = Artist.new(:name => params[:artist_name])
+      album  = Album.new(:artist => artist, :name => params[:album_name])
+      album.songs.each{|song| PlayQueue.add(song, current_user)}
+    end
+
+    deliver_json(200, {:songs => PlayQueue.songs.collect(&:to_hash)})
   end
 
   def remove
+    artist = Artist.new(:name => params[:artist_name])
+    song = artist.songs.find{|song| song.title == params[:song_name]}
+    PlayQueue.remove(song,current_user)
+
+    deliver_json(200, {:songs => PlayQueue.songs.collect(&:to_hash)})
   end
 
   def clear
+    Play.mpd.clear
+
+    deliver_json(200, {:songs => PlayQueue.songs.collect(&:to_hash)})
   end
 
 
