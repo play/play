db9ac09f8b1276635118e3f6e1ddbbffe479f495
Jon Maddox <jon@jonmaddox.com>
2013-08-08 22:13:47 -0400

    advertise via bonjour in production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Play::Application.initialize!
+
+
+if Rails.env.production?
+  DNSSD.register Socket.gethostname, '_play._tcp', nil, 3030
+end
