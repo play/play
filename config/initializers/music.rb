4c042a1f634ccb018da1d812eacb5bcbe636eed6
Jon Maddox <jon@jonmaddox.com>
2013-09-16 16:17:30 -0400

    kill play.rb from initializers and move things elsewhere
    The module-esque things need to go to lib, and the mpd stuff will go
    under music.rb when we boot up all the channels

--- config/initializers/music.rb
+++ config/initializers/music.rb
@@ -2,4 +2,17 @@
 
 Channel.all.each do |channel|
   channel.start
+
+  if !Rails.env.test? && channel.mpd
+
+    # Set up mpd to natively consume songs
+    channel.mpd.repeat  = true
+    channel.mpd.consume = true
+
+    # Scan for new songs just in case
+    channel.mpd.update
+
+    # Play the tunes
+    channel.mpd.play
+  end
 end
--- lib/play.rb
+++ lib/play.rb
@@ -5,6 +5,39 @@ require "play/speaker"
 
 module Play
 
+  # Our connection to MPD.
+  #
+  # Returns an instance of MPD.
+  def self.mpd
+    return nil if !defined?(Channel)
+    @mpd ||= Channel.first && Channel.first.mpd
+  end
+
+  # mpd only really knows about the relative path to songs:
+  # Justice/Cross/Stress.mp3, for example. We need to know the path before
+  # that for a few things (reading in the MP3 tag data, for one). This method
+  # reads the path from your config/mpd.conf and loads up the value you have
+  # for `music_directory`.
+  #
+  # Returns a String.
+  def self.music_path
+    config = File.read('config/mpd.conf')
+    config.scan(/^(?<!#)\s*music_directory\s+"([^"]*)"$/).last.first
+  end
+
+  # Directory where cached album art images will be stored.
+  def self.album_art_cache_path
+    'public/images/art'
+  end
+
+  # The config file of Play. Contains things like keys, database config, and
+  # who shot JFK.
+  #
+  # Returns a Hash.
+  def self.config
+    @config ||= YAML::load(File.open('config/play.yml'))
+  end
+
   # Local instances of Play Speakers found on the network
   #
   # Returns an array of Speaker objects.
