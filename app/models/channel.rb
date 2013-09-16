486bfdd01f605a25e08c294e109665ee25f2f343
Jon Maddox <jon@jonmaddox.com>
2013-09-16 14:29:23 -0400

    write out the mpd config file for the channel

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,9 +1,7 @@
 class Channel < ActiveRecord::Base
   has_many :users
-
-  def write_config
-    # write out config based on mpd_port, httpd_port, and config_path
-  end
+  
+  attr_accessible :mpd_port, :httpd_port, :color, :name
 
   def start
     # start this channel's mpd instance
@@ -12,5 +10,35 @@ class Channel < ActiveRecord::Base
   def stop
     # shut down this channel's mpd instance
   end
+
+  def config_root_path
+    File.join(Play.config['mpd']['config_root_path'], "channel-#{id}")
+  end
+
+  def config_path
+    File.join(config_root_path, 'mpd.conf')
+  end
+  
+  def write_config
+    template_path = File.join(Rails.root, 'templates', 'mpd.conf.erb')
+    
+    opts = OpenStruct.new(:channel_name => name,
+                          :httpd_port => httpd_port,
+                          :mpd_port => mpd_port,
+                          :music_path => Play.config['mpd']['music_path'],
+                          :stream_bitrate => Play.config['mpd']['stream_bitrate'],
+                          :system_audio => Play.config['mpd']['system_audio'],
+                          :mpd_config_root_path => Play.config['mpd']['config_root_path'],
+                          :channel_config_root_path => config_root_path
+                          )
+
+    template = open(template_path, 'r') {|f| f.read}
+    
+    # ensure path exists
+    FileUtils.mkdir_p(config_root_path)
+    
+    File.open(config_path, 'w') {|f| f.write(ERB.new(template).result(opts.instance_eval {binding})) }
+  end
+
 
 end
