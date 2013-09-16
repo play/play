9a8a5f885965f6aafc558503e7672f8e59e07c48
Daniel Hengeveld <danielwh@gmail.com>
2013-09-16 14:08:59 -0400

    fix callbacks on channel, get tests to pass

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -5,33 +5,33 @@ class Channel < ActiveRecord::Base
   after_save :write_config, :restart_mpd
 
   # Hooks
-  def set_ports(record)
+  def set_ports
     # This is slow but that's OK, how often are you going to create channels?
-    unless record.mpd_port
+    unless self.mpd_port
       mpd_port = 6600 + Channel.count
       while Channel.where(:mpd_port => mpd_port).count > 0 do
         mpd_port += 1
       end
-      record.mpd_port = mpd_port
+      self.mpd_port = mpd_port
     end
 
-    unless record.httpd_port
+    unless self.httpd_port
       httpd_port = 8000 + Channel.count
       while Channel.where(:httpd_port => httpd_port).count > 0 do
         httpd_port += 1
       end
-      record.httpd_port = httpd_port
+      self.httpd_port = httpd_port
     end
   end
 
-  def write_config(record)
-    unless record.config_path
-      record.config_path = "#{RAILS_ROOT}/tmp/mpd-#{record.id}/mpd.conf"
-      record.save
+  def write_config
+    unless self.config_path
+      self.config_path = "#{RAILS_ROOT}/tmp/mpd-#{self.id}/mpd.conf"
+      self.save
     end
   end
 
-  def restart_mpd(record)
+  def restart_mpd
   end
 
   def start
