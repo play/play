2fa5cce3d2c5e3d81bf9b5a10665190bedf23e30
Jon Maddox <jon@jonmaddox.com>
2013-09-18 09:28:06 -0400

    fix config paths

--- test/models/channel_test.rb
+++ test/models/channel_test.rb
@@ -6,7 +6,7 @@ class ChannelTest < ActiveSupport::TestCase
     setup do
       @channel = Channel.create(:name => 'Floor 2', :color => 'red')
     end
-    
+
     test "generated an mpd_port" do
       assert_equal 6601, @channel.mpd_port
     end
@@ -20,20 +20,18 @@ class ChannelTest < ActiveSupport::TestCase
     setup do
       @channel = Channel.make
     end
-    
+
     test 'has the right config_directory' do
-      assert_equal  "#{Rails.root}/test/tmp/mpds/channel-1", @channel.config_directory
+      assert_equal  "#{Rails.root}/test/tmp/play-test/channels/channel-1", @channel.config_directory
     end
 
     test 'has the right mpd config path' do
-      assert_equal  "#{Rails.root}/test/tmp/mpds/channel-1/mpd.conf", @channel.config_path
+      assert_equal  "#{Rails.root}/test/tmp/play-test/channels/channel-1/mpd.conf", @channel.config_path
     end
-    
+
     test 'writes out the mpd config' do
       @channel.write_config
       assert File.exists?(@channel.config_path)
     end
-    
   end
-  
 end
