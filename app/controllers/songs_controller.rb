c8a175d59b8c48923815d1084741eca4f6437265
Jon Maddox <jon@jonmaddox.com>
2013-09-18 20:15:24 -0400

    use default mpd for this

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,13 +20,13 @@ class SongsController < ApplicationController
     File.rename song.path, File.join(path, File.basename(song.path))
 
     # Update the index
-    current_mpd.update
+    Play.mpd.update
 
     render :text => 'Uploaded!'
   end
 
   def search
-    if record = current_mpd.send_command(:find, :artist, params[:q]).first
+    if record = Play.mpd.send_command(:find, :artist, params[:q]).first
       return redirect_to(artist_path(record[:artist]))
     end
 
