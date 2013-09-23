b20a7be3ec217edfb182ffe0962f6017887b5aed
Daniel Hengeveld <danielwh@gmail.com>
2013-09-23 08:45:19 -0700

    remove misleading comment

--- lib/play/connection_pool.rb
+++ lib/play/connection_pool.rb
@@ -12,7 +12,7 @@ class ConnectionPool
     return conn if conn && conn.connected?
 
     conn = MPD.new('localhost', channel.mpd_port)
-    conn.connect #callback thread enabled
+    conn.connect
     @channels[channel.id] = conn
     conn
   end
