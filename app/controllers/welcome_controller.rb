2d4808f08f1157bad059922e4a4be71d0635fe47
Daniel Hengeveld <danielwh@gmail.com>
2013-09-25 18:37:42 -0700

    if there's only one channel, go straight to the queue

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    if Channel.count == 1
+      return redirect_to channel_path(Channel.first)
+    end
     @channels = Channel.all
     render :layout => 'welcome'
   end
