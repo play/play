3ac0043e9f2a16ff71e5ef9c51604c5d23f6aaa9
Josh Nichols <technicalpickles@github.com>
2013-09-29 14:08:31 -0400

    Less output=

--- app/controllers/api/commands_controller.rb
+++ app/controllers/api/commands_controller.rb
@@ -8,100 +8,103 @@ class Api::CommandsController < Api::BaseController
                 .gsub(/\s+/, " ")         # make sure all spaces, no newlines or tabs, and no doubles
                 .gsub(/^(\/|hubot )/, '') # strip leading / or hubt
 
-    case command
-    when /^play$/
-      channel.mpd.play
-      output = "You got it, playing"
-    when /^(?:play )?pause/i
-      channel.mpd.pause = true
-      output = "You got it, pausing"
-    when /^(?:play )?(sup|what'?s playing)\??$/i
-      output = try_now_playing channel do |song|
-        %{Now playing on #{channel.name}: "#{song.title}" by #{song.artist_name}, from "#{song.album_name}"}
-      end
-    when /^(?:play )?next$/i
-      output = try_next_song channel do |next_song|
-        %{On to next one on #{channel.name}: "#{next_song.title}" by #{next_song.artist_name}, from "#{next_song.album_name}"}
-      end
-    when /^(?:play )?(what's next\??|next\?)$/i
-      output = try_next_song channel do |next_song|
-        %{Up next on #{channel.name}: "#{next_song.title}" by #{next_song.artist_name}, from "#{next_song.album_name}"}
-      end
+    output = case command
+             when /^play$/
+               channel.mpd.play
+               "You got it, playing"
+             when /^(?:play )?pause/i
+               channel.mpd.pause = true
+               "You got it, pausing"
+             when /^(?:play )?(sup|what'?s playing)\??$/i
+               try_now_playing channel do |song|
+                 %{Now playing on #{channel.name}: "#{song.title}" by #{song.artist_name}, from "#{song.album_name}"}
+               end
+             when /^(?:play )?next$/i
+               try_next_song channel do |next_song|
+                 channel.mpd.next
+                 %{On to next one on #{channel.name}: "#{next_song.title}" by #{next_song.artist_name}, from "#{next_song.album_name}"}
+               end
+             when /^(?:play )?(what's next\??|next\?)$/i
+               try_next_song channel do |next_song|
+                 %{Up next on #{channel.name}: "#{next_song.title}" by #{next_song.artist_name}, from "#{next_song.album_name}"}
+               end
 
-    when /^(?:play )?album (.*) by (.*)$/i
-      album_name = $1
-      artist_name = $2
+             when /^(?:play )?album (.*) by (.*)$/i
+               album_name = $1
+               artist_name = $2
 
-      artist_name = channel.mpd.search(:artist, artist_name, :case_sensitive => false).first.try(:artist)
-      artist = Artist.new(:name => artist_name)
+               artist = Artist.new(:name => artist_name)
+               album = artist.albums.select { |album| album.name.downcase == album_name.downcase }.first
 
-      album = artist.albums.select { |album| album.name.downcase == album_name.downcase }.first
+               if album
+                 queue_songs(channel, user, album.songs)
+               else
+                 %{Couldn't find album "#{album_name}" by #{artist_name} ;_;}
+               end
+             when /^(?:play )?artist (.*)$/i
+               artist_name = $1
+               artist = Artist.new(:name => artist_name)
 
-      output = queue_songs(channel, user, album.songs)
-    when /^(?:play )?artist (.*)$/i
-      artist_name = $1
-      artist = Artist.new(:name => artist_name)
+               songs = artist.songs.sample(3)             
 
-      songs = artist.songs.sample(3)             
-
-      output = if songs.any?
+               if songs.any?
                  queue_songs(channel, user, songs)
                else
                  "Didn't find any songs by #{artist_name} ¯\_(ツ)_/¯"
                end
-    when /^(?:play )?(.*) by (.*)$/i
-      song_name = $1
-      artist_name = $2
+             when /^(?:play )?(.*) by (.*)$/i
+               song_name = $1
+               artist_name = $2
 
-      artist = Artist.new(:name => artist_name)
-      song = artist.songs.find{|song| song.title.downcase == song_name.downcase}
+               artist = Artist.new(:name => artist_name)
+               song = artist.songs.find{|song| song.title.downcase == song_name.downcase}
 
-      output = if song
+               if song
                  queue_song(channel, user, song)
                else
                  %{Can't find "#{song_name}" by #{artist_name}}
                end
-    when /^(?:play )?I want this song$/i
-      output = try_now_playing channel do |song|
-        "Pretty rad, innit? Grab it for yourself: #{song_url(:artist_name => song.artist_name, :title => song.title)}"
-      end
-    when /^(?:play )?I want this album$/i
-      output = try_now_playing channel do |song|
-        if song.album_name
-          "dope beats available here: #{album_url(:artist_name => song.artist_name, :name => song.album_name)}"
-        else
-          %{"#{song.title}" by #{song.artist_name} isn't actually part of an album. Maybe you want just this song instead?}
-        end
-      end
-    when /^(?:play )?(something i('d)? like)|(the good shit)$/i
-      songs = user.likes.limit(3).order('rand()').collect(&:song)
+             when /^(?:play )?I want this song$/i
+               try_now_playing channel do |song|
+                 "Pretty rad, innit? Grab it for yourself: #{song_url(:artist_name => song.artist_name, :title => song.title)}"
+               end
+             when /^(?:play )?I want this album$/i
+               try_now_playing channel do |song|
+                 if song.album_name
+                   "dope beats available here: #{album_url(:artist_name => song.artist_name, :name => song.album_name)}"
+                 else
+                   %{"#{song.title}" by #{song.artist_name} isn't actually part of an album. Maybe you want just this song instead?}
+                 end
+               end
+             when /^(?:play )?(something i('d)? like)|(the good shit)$/i
+               songs = user.likes.limit(3).order('rand()').collect(&:song)
 
-      output = queue_songs(channel, user, songs)
-    when /^(?:play )?I (like|star|love|dig) this( song)?$/i 
-      output = try_now_playing channel do |song|
-        user.like(song.path)
-        output = %{You like #{song.artist_name}'s "#{song.title}", too? Awesome.}
-      end
-    when /^(?:play )?volume on (.*)$/i
-      #  authedRequest message, "/speakers", 'get', {}, (err, res, body) ->
-      #    json = JSON.parse(body)['speakers']
-      #    speakers = json.filter (x) -> x['name'] == "play-#{speaker}"
-      #    volume = speakers[0]['volume']
-      #    message.send("Yo :#{message.message.user.name}:, the volume is #{volume} :mega:")
-      output = "Still need to implement volume check (#{command.inspect}), lol"
-    when /^(?:play )?volume (.*) (.*)$/i 
-      #  params = { speaker_name: speaker, level: volume }
-      #  authedRequest message, "/speakers/#{speaker}/volume", 'post', params, (err, res, body) ->
-      #    if msg=JSON.parse(body)['message']
-      #      message.send(msg)
-      #    else
-      #      message.send("Bumped the volume to #{JSON.parse(body)['volume']}")
-      output = "Still need to implement volume adjust (#{command.inspect}), lol"
-    when /^(?:play )?where'?s play$/i
-      output = "#{channel.name} is at #{channel_url(channel)}, and can be streamed from #{api_channel_stream_url(channel)}"
-    else
-      output = "lol wut? #{command.inspect} doesn't even seem like a thing Play can do"
-    end
+               queue_songs(channel, user, songs)
+             when /^(?:play )?I (like|star|love|dig) this( song)?$/i 
+               try_now_playing channel do |song|
+                 user.like(song.path)
+                 output = %{You like #{song.artist_name}'s "#{song.title}", too? Awesome.}
+               end
+             when /^(?:play )?volume on (.*)$/i
+               #  authedRequest message, "/speakers", 'get', {}, (err, res, body) ->
+               #    json = JSON.parse(body)['speakers']
+               #    speakers = json.filter (x) -> x['name'] == "play-#{speaker}"
+               #    volume = speakers[0]['volume']
+               #    message.send("Yo :#{message.message.user.name}:, the volume is #{volume} :mega:")
+               "Still need to implement volume check (#{command.inspect}), lol"
+             when /^(?:play )?volume (.*) (.*)$/i 
+               #  params = { speaker_name: speaker, level: volume }
+               #  authedRequest message, "/speakers/#{speaker}/volume", 'post', params, (err, res, body) ->
+               #    if msg=JSON.parse(body)['message']
+               #      message.send(msg)
+               #    else
+               #      message.send("Bumped the volume to #{JSON.parse(body)['volume']}")
+               "Still need to implement volume adjust (#{command.inspect}), lol"
+             when /^(?:play )?where'?s play$/i
+               "#{channel.name} is at #{channel_url(channel)}, and can be streamed from #{api_channel_stream_url(channel)}"
+             else
+               "lol wut? #{command.inspect} doesn't even seem like a thing Play can do"
+             end
 
     render :text => output
   end
