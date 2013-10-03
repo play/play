1dff92ea0fb4edf2628ae057de35f0920d6d4ae5
Josh Nichols <technicalpickles@github.com>
2013-10-03 00:28:56 -0400

    Support /p

--- app/controllers/api/commands_controller.rb
+++ app/controllers/api/commands_controller.rb
@@ -24,27 +24,27 @@ class Api::CommandsController < Api::BaseController
 
   def execute(channel, user, command)
     case command
-    when /^play$/
+    when /^p(?:lay)?$/
       channel.mpd.play
       "You got it, playing"
-    when /^(?:play )?pause/i
+    when /^(?:p(?:lay)? )?pause/i
       channel.mpd.pause = true
       "You got it, pausing"
-    when /^(?:play )?(sup|what'?s playing)\??$/i
+    when /^(?:p(?:lay)? )?(sup|what'?s playing)\??$/i
       try_now_playing channel do |song|
         %{Now playing on #{channel.name}: "#{song.title}" by #{song.artist_name}, from "#{song.album_name}"}
       end
-    when /^(?:play )?next$/i
+    when /^(?:p(?:lay)? )?next$/i
       try_next_song channel do |next_song|
         channel.mpd.next
         %{On to next one on #{channel.name}: "#{next_song.title}" by #{next_song.artist_name}, from "#{next_song.album_name}"}
       end
-    when /^(?:play )?(what's next\??|next\?)$/i
+    when /^(?:p(?:lay)? )?(what's next\??|next\?)$/i
       try_next_song channel do |next_song|
         %{Up next on #{channel.name}: "#{next_song.title}" by #{next_song.artist_name}, from "#{next_song.album_name}"}
       end
 
-    when /^(?:play )?album (.*) by (.*)$/i
+    when /^(?:p(?:lay)? )?album (.*) by (.*)$/i
       album_name = $1
       artist_name = $2
 
@@ -63,7 +63,7 @@ class Api::CommandsController < Api::BaseController
       else
         %{Couldn't find album "#{album_name}" by #{artist_name} ;_;}
       end
-    when /^(?:play )?artist (.*)$/i
+    when /^(?:p(?:lay)? )?artist (.*)$/i
       artist_name = $1
       artist = Artist.new(:name => artist_name)
 
@@ -74,7 +74,7 @@ class Api::CommandsController < Api::BaseController
       else
         "Didn't find any songs by #{artist_name} ¯\_(ツ)_/¯"
       end
-    when /^(?:play )?(.*) by (.*)$/i
+    when /^(?:p(?:lay)? )?(.*) by (.*)$/i
       song_name = $1
       artist_name = $2
 
@@ -86,11 +86,11 @@ class Api::CommandsController < Api::BaseController
       else
         %{Can't find "#{song_name}" by #{artist_name}}
       end
-    when /^(?:play )?I want this song$/i
+    when /^(?:p(?:lay)? )?I want this song$/i
       try_now_playing channel do |song|
         "Pretty rad, innit? Grab it for yourself: #{song_url(:artist_name => song.artist_name, :title => song.title)}"
       end
-    when /^(?:play )?I want this album$/i
+    when /^(?:p(?:lay)? )?I want this album$/i
       try_now_playing channel do |song|
         if song.album_name
           "dope beats available here: #{album_url(:artist_name => song.artist_name, :name => song.album_name)}"
@@ -98,23 +98,23 @@ class Api::CommandsController < Api::BaseController
           %{"#{song.title}" by #{song.artist_name} isn't actually part of an album. Maybe you want just this song instead?}
         end
       end
-    when /^(?:play )?(something i('d)? like)|(the good shit)$/i
+    when /^(?:p(?:lay)? )?(something i('d)? like)|(the good shit)$/i
       songs = user.likes.limit(3).order('rand()').collect(&:song)
 
       queue_songs(channel, user, songs)
-    when /^(?:play )?I (like|star|love|dig) this( song)?$/i
+    when /^(?:p(?:lay)? )?I (like|star|love|dig) this( song)?$/i
       try_now_playing channel do |song|
         user.like(song.path)
         output = %{You like #{song.artist_name}'s "#{song.title}", too? Awesome.}
       end
-    when /^(?:play )?volume on (.*)$/i
+    when /^(?:p(?:lay)? )?volume on (.*)$/i
       #  authedRequest message, "/speakers", 'get', {}, (err, res, body) ->
       #    json = JSON.parse(body)['speakers']
       #    speakers = json.filter (x) -> x['name'] == "play-#{speaker}"
       #    volume = speakers[0]['volume']
       #    message.send("Yo :#{message.message.user.name}:, the volume is #{volume} :mega:")
       "Still need to implement volume check (#{command.inspect}), lol"
-    when /^(?:play )?volume (.*) (.*)$/i
+    when /^(?:p(?:lay) )?volume (.*) (.*)$/i
       #  params = { speaker_name: speaker, level: volume }
       #  authedRequest message, "/speakers/#{speaker}/volume", 'post', params, (err, res, body) ->
       #    if msg=JSON.parse(body)['message']
@@ -122,11 +122,11 @@ class Api::CommandsController < Api::BaseController
       #    else
       #      message.send("Bumped the volume to #{JSON.parse(body)['volume']}")
       "Still need to implement volume adjust (#{command.inspect}), lol"
-    when /^(?:play )?where'?s play$/i
+    when /^(?:p(?:lay)? )?where'?s play$/i
       "#{channel.name} is at #{channel_url(channel)}, and can be streamed from #{api_channel_stream_url(channel)}"
-    when /^(?:play )?list channels$/i
+    when /^(?:p(?:lay)? )?list channels$/i
       "These are the channels I know: #{Channel.all.map(&:name).join(', ')}"
-    when /^(?:play )?create channel (.*)$/i
+    when /^(?:p(?:lay)? )?create channel (.*)$/i
       name = $1
       begin
         channel = Channel.create!(:name => name)
