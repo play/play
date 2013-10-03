707cc0274e438de6a164baab7b49e5a5ad770e1b
Jon Maddox <jon@jonmaddox.com>
2013-10-03 00:48:19 -0400

    turn caching back off in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Play::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
