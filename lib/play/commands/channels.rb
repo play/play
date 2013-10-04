4e4b62931fd1f3d73bc3995d8d836457c76be226
Jon Maddox <jon@jonmaddox.com>
2013-10-03 23:50:23 -0400

    we can't get to urls here

--- lib/play/commands/channels.rb
+++ lib/play/commands/channels.rb
@@ -17,10 +17,10 @@ module Play
           name = $1
           begin
             channel = Channel.create!(:name => name)
-            "Ok, I created your channel! You can see it here: #{channel_url(channel)}. Or stream it from one of the great Play apps."
+            "Ok, I created your channel! You can browse it on Play or you can stream it from one of the great Play apps."
           rescue ActiveRecord::RecordInvalid
             channel = Channel.find_by_name(name)
-            "Ooops! That channel already exist. You can see it here: #{channel_url(channel)}. Or stream it from one of the great Play apps."
+            "Ooops! That channel already exist. You can browse it on Play or you can stream it from one of the great Play apps."
           end
         end
       end
